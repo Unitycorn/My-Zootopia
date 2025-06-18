d05109b57d857ca4f27ffcf88294c8dbd81bde0e
Unitycorn <flaig.m@gmx.de>
2025-06-18 14:36:00 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,5 +1,9 @@
 import json
 
+HTML_TEMPLATE_FILE = 'animals_template.html'
+NEW_HML_FILE = 'animals.html'
+REPLACE_STRING = "__REPLACE_ANIMALS_INFO__"
+
 
 def load_data(file_path):
     """ Loads a JSON file """
@@ -7,19 +11,36 @@ def load_data(file_path):
         return json.load(handle)
 
 
-animals_data = load_data('animals_data.json')
+def load_html_template():
+    """ Loads a HTML template file """
+    html_data = open(HTML_TEMPLATE_FILE, "r")
+    html_content = html_data.read()
+    html_data.close()
+    return html_content
 
-for animal in animals_data:
-    name = animal['name']  # Assuming name is always given
-    print(f"Name: {name}")
-    if 'diet' in animal['characteristics'].keys():
-        diet = animal['characteristics']['diet']
-        print(f"Diet: {diet}")
-    if 'locations' in animal.keys():
-        print(f"Location: {animal['locations'][0]}")
-        # print(", ".join(animal['locations'])) <- all locations
-    if 'type' in animal['characteristics'].keys():
-        animal_type = animal['characteristics']['type']
-        print(f"Type: {animal_type}")
-    print()
 
+def get_animals_info():
+    """ Reads information from JSON file and returns them as a string"""
+    output = ''
+    animals_data = load_data('animals_data.json')
+    for animal in animals_data:
+        name = animal['name']  # Assuming name is always given
+        output += f"Name: {name}\n"
+        if 'diet' in animal['characteristics'].keys():
+            diet = animal['characteristics']['diet']
+            output += f"Diet: {diet}\n"
+        if 'locations' in animal.keys():
+            output += f"Location: {animal['locations'][0]}\n"
+            # print(", ".join(animal['locations'])) <- all locations
+        if 'type' in animal['characteristics'].keys():
+            animal_type = animal['characteristics']['type']
+            output += f"Type: {animal_type}\n"
+        output += "\n"
+    return output
+
+
+html_content_with_animals = load_html_template().replace(REPLACE_STRING, get_animals_info())
+
+with open(NEW_HML_FILE, 'w') as file:
+    file.write(html_content_with_animals)
+file.close()
